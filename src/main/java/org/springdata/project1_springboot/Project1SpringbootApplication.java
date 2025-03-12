4e2574f74b97eb3d3c01399b91b73c4ad4590bc2
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-12 09:58:02 +0000

    add the project spring boot to the repository

---
+++ src/main/java/org/springdata/project1_springboot/Project1SpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.springdata.project1_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project1SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springdata/project1_springboot/Project1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdata.project1_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
