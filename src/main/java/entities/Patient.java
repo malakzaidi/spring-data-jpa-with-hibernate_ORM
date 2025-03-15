338fc36b5f53c93b5b04ee6801a435db27f6cbca
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-15 16:31:55 +0000

    create the entity JPA Patient

---
+++ src/main/java/entities/Patient.java
@@ -0,0 +1,22 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String Nom;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+
+
+
+}
