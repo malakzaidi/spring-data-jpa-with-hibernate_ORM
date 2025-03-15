6ef30915b747b9e606b392fe36271d21e0179572
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-15 21:20:50 +0000

    add the JPA repository for managing patients

---
+++ src/main/java/repositories/PatientRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
