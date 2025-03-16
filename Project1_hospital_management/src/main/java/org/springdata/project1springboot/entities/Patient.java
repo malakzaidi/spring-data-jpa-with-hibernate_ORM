868caa61d6c7dfa4e2096712c186b8e40ff2e806
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-16 18:38:03 +0000

    add the entity Patient

---
+++ Project1_hospital_management/src/main/java/org/springdata/project1springboot/entities/Patient.java
@@ -0,0 +1,21 @@
+package org.springdata.project1springboot.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+
+}
