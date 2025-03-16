af21c32e406151429c60535b15b5d89f77bd9182
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-16 20:34:20 +0000

    add the second project to the repository

---
+++ Project2_Many_To_Many_Use_Case/src/main/java/org/springdata/project2_many_to_many_use_case/Project2ManyToManyUseCaseApplication.java
@@ -0,0 +1,13 @@
+package org.springdata.project2_many_to_many_use_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2ManyToManyUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2ManyToManyUseCaseApplication.class, args);
+	}
+
+}
