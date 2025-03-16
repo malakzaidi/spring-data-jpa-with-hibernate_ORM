2115f8eded86525def39d98756ac4318b5e74c46
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-16 18:10:33 +0000

    add the project to the repository

---
+++ Project1_hospital_management/src/main/java/org/springdata/project1springboot/Project1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.springdata.project1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1SpringBootApplication.class, args);
+	}
+
+}
---
+++ Project1_hospital_management/src/test/java/org/springdata/project1springboot/Project1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdata.project1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
